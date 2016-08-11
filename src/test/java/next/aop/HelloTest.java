375e316a33dda9ea847e60df856c0af079a61777
ez2fly <ez2fly@naver.com>
2016-08-11 21:20:07 +0900

    Spring 의 ProxyFactoryBean

--- src/test/java/next/aop/HelloTest.java
+++ src/test/java/next/aop/HelloTest.java
@@ -1,12 +1,20 @@
 package next.aop;
 
-import static org.junit.Assert.*;
-
 import java.lang.reflect.Proxy;
 
 import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 public class HelloTest {
+	
+	public void proxyFactoryBean() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		Hello proxiedHello = (Hello) pfBean.getObject();
+		System.out.println(proxiedHello.sayHello("jae sung"));
+		System.out.println(proxiedHello.sayHi("joo han"));
+	}
 
 	@Test
 	public void hello() throws Exception {
---
+++ src/test/java/next/aop/UppercaseAdvice.java
@@ -0,0 +1,13 @@
+package next.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+class UppercaseAdvice implements MethodInterceptor {
+@Override
+	public Object invoke(MethodInvocation arg0) throws Throwable {
+		// TODO Auto-generated method stub
+		String ret = (String) arg0.proceed();
+		return ret.toUpperCase();
+	}
+}
